45fefb752f5cf6c7203c25714df6b8d7f158324b
Colin Hebert <hebert.colin@gmail.com>
2012-09-17 23:04:47 +0200

    Raise an ArgumentError if no configuration loader has been defined

--- lib/tmuxpowerline.rb
+++ lib/tmuxpowerline.rb
@@ -1,7 +1,7 @@
 class TmuxPowerline
   def initialize(configuration_loader)
     if configuration_loader.nil?
-      raise "The configuration loader can't be nil"
+      raise ArgumentError, "The configuration loader can't be nil"
     end
     @configuration_loader = configuration_loader
   end
--- test/test_tmuxpowerline.rb
+++ test/test_tmuxpowerline.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 class TestTmuxPowerline < Test::Unit::TestCase
   should "raise an exception if a configuration loader is given as nil" do
-    assert_raise do
+    assert_raise ArgumentError do
       TmuxPowerline.new nil
     end
   end
