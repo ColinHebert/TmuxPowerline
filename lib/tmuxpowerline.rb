1adce3e8f973fb6da22a1546fe9611bcf50e1e2c
Colin Hebert <hebert.colin@gmail.com>
2012-09-18 02:04:20 +0200

    Define a default configuration loader and add a method to load the configuration

--- lib/tmuxpowerline.rb
+++ lib/tmuxpowerline.rb
@@ -1,10 +1,14 @@
 require 'configuration/yaml'
 
 class TmuxPowerline
-  def initialize(configuration_loader)
+  def initialize(configuration_loader=Configuration::Yaml.new)
     if configuration_loader.nil?
       raise ArgumentError, "The configuration loader can't be nil"
     end
     @configuration_loader = configuration_loader
+  end
+
+  def load_config(config)
+    @configuration_loader.load_config(config)
   end
 end
