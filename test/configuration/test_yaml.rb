506a6c6d87c20eea50004904e7c2f71ac58cdde5
Colin Hebert <hebert.colin@gmail.com>
2012-09-18 02:33:46 +0200

    Simplify yaml tests and configuration

--- test/configuration/test_yaml.rb
+++ test/configuration/test_yaml.rb
@@ -4,77 +4,95 @@ class TestYaml < Test::Unit::TestCase
   setup do
     @configuration=Configuration::Yaml.new
     @configuration.load_config <<-CONFIG
-      section-1:
+      existing-section: {}
+
+      section-with-orientation:
         orientation: RTL
-        segments:
-          - type: empty
-            style: {type: thick, bg_color: green, fg_color: black}
-          - type: plain
-            max_length: 10
-            fake_property: lol
-            style: {type: thin, fg_color: blue}
-          - type: empty
-            style: {type: thin}
-
-      section-2:
-        segments:
-          - type: empty
-            style: {type: thin}
-
-      status-right:
-        segments:
-          - type: empty
-            style: {type: thin}
-
       status:
         orientation: LTR
+
+      section-without-orientation: {}
+      status-right: {}
+
+      section-with-segments:
+        segments:
+          - type: empty
+            style: {type: thick}
+          - type: empty
+            style: {type: thick}
+          - type: empty
+            style: {type: thick}
+      section-without-segment: {}
+
+      segment-types:
+        segments:
+          - type: empty
+            style: {type: thick}
+          - type: plain
+            style: {type: thick}
+
+      segment-properties:
+        segments:
+          - type: empty
+            style: {type: thick}
+            max_length: 10
+
+      segment-styles:
+        segments:
+          - type: empty
+            style: {type: thick}
+          - type: empty
+            style: {type: thin}
+
+      style-properties:
+        segments:
+          - type: empty
+            style: {type: thick, bg_color: green, fg_color: blue}
     CONFIG
   end
 
   should "return true if the section exists, false otherwise" do
-    assert @configuration.section_exists? 'section-1'
-    assert !@configuration.section_exists?('fake_section')
+    assert @configuration.section_exists? 'existing-section'
+    assert !@configuration.section_exists?('fake-section')
   end
 
   should "raise an exception if the chosen section doesn't exist" do
     assert_raise UndefinedSectionError do
-      @configuration.get_section 'fake_section'
+      @configuration.get_section 'fake-section'
     end
   end
 
   should "obtain a section orientation if it's defined" do
-    assert_equal Configuration::ORIENTATIONS[:RTL], @configuration.get_orientation('section-1')
+    assert_equal Configuration::ORIENTATIONS[:RTL], @configuration.get_orientation('section-with-orientation')
     assert_equal Configuration::ORIENTATIONS[:LTR], @configuration.get_orientation('status')
   end
 
   should "define the default section orientation if none has been defined" do
-    assert_equal Configuration::ORIENTATIONS[:LTR], @configuration.get_orientation('section-2')
+    assert_equal Configuration::ORIENTATIONS[:LTR], @configuration.get_orientation('section-without-orientation')
     assert_equal Configuration::ORIENTATIONS[:RTL], @configuration.get_orientation('status-right')
   end
 
   should "obtain the correct number of segments, even if the property isn't defined" do
-    assert_equal 3, @configuration.get_section('section-1').size
-    assert_equal 1, @configuration.get_section('status-right').size
-    assert_equal 1, @configuration.get_section('section-2').size
-    assert_equal 0, @configuration.get_section('status').size
+    assert_equal 3, @configuration.get_section('section-with-segments').size
+    assert_equal 0, @configuration.get_section('section-without-segment').size
   end
 
   should "generate segments based on the type defined in the configuration" do
-    assert @configuration.get_section('section-1')[0].is_a?(Segment::Empty)
-    assert @configuration.get_section('section-1')[1].is_a?(Segment::Plain)
+    assert @configuration.get_section('segment-types')[0].is_a?(Segment::Empty)
+    assert @configuration.get_section('segment-types')[1].is_a?(Segment::Plain)
   end
 
   should "set automatically the properties for each segment" do
-    assert_equal 10, @configuration.get_section('section-1')[1].max_length
+    assert_equal 10, @configuration.get_section('segment-properties')[0].max_length
   end
 
   should "set the segment style" do
-    assert @configuration.get_section('section-1')[0].style.is_a?(Style::Thick)
-    assert @configuration.get_section('section-1')[1].style.is_a?(Style::Thin)
+    assert @configuration.get_section('segment-styles')[0].style.is_a?(Style::Thick)
+    assert @configuration.get_section('segment-styles')[1].style.is_a?(Style::Thin)
   end
 
   should "set the segment style properties automatically" do
-    assert_equal 'green', @configuration.get_section('section-1')[0].style.bg_color
-    assert_equal 'blue', @configuration.get_section('section-1')[1].style.fg_color
+    assert_equal 'green', @configuration.get_section('style-properties')[0].style.bg_color
+    assert_equal 'blue', @configuration.get_section('style-properties')[0].style.fg_color
   end
 end
