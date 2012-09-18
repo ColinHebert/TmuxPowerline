b1d3ad45c04bcff3c2868b4e4c10830c5ca1d3c8
Colin Hebert <hebert.colin@gmail.com>
2012-09-18 18:29:18 +0200

    Define Thick and Thin to use the right separator

--- lib/style/thick.rb
+++ lib/style/thick.rb
@@ -1,5 +1,8 @@
-require 'style/style'
+# encoding: UTF-8
+require 'style/separator'
 
-class Style::Thick < Style
-
+class Style::Thick < Style::Separator
+  def initialize
+    super '⮀', '⮂'
+  end
 end
--- lib/style/thin.rb
+++ lib/style/thin.rb
@@ -1,5 +1,8 @@
-require 'style/style'
+# encoding: UTF-8
+require 'style/separator'
 
-class Style::Thin < Style
-
+class Style::Thin < Style::Separator
+  def initialize
+    super '⮁', '⮃'
+  end
 end
